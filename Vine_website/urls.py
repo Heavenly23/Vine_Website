85fd912db9d002d3a891c48af4c2fdb9f7abdfc1
merry <mary.hailu23@gmail.com>
2020-11-25 15:52:25 -0500

    my commit

--- Vine_website/urls.py
+++ Vine_website/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('vine/', include('Vine.urls')),
+    path('', include('Vine.urls')),
 
 ]
 
