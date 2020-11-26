d3c1de9bf39343011c05ff50ba5938b083952b91
merry <mary.hailu23@gmail.com>
2020-11-26 12:20:04 -0500

    commit

--- Vine_website/Vine/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from . models import VineAlbum,Vine,Profile
-# Register your models here.
-
-admin.site.register(VineAlbum)
-admin.site.register(Vine)
-admin.site.register(Profile)
--- Vine_website/Vine/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class VineConfig(AppConfig):
-    name = 'Vine'
